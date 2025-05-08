d73912ad05a252a7b1cfc74cb3b0aa382178f923
Kadeem Austin <dev@kadeemaustin.ai>
2025-05-08 07:53:07 -0400

    refactor: move embedding to standalone module

--- prism/model.py
+++ prism/model.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from prism.embedding import Embedding
 
 
 class Model(nn.Module):
@@ -20,31 +21,6 @@ class Model(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.positional_encoding(self.embedding(x))
 
-
-class Embedding(nn.Module):
-    def __init__(self, vocab_size: int, embedding_size: int):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_size)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Returns the embedding for the given token ids.
-
-        `x` is expected to be a tensor of shape (..., batch_size) where each 
-        element is an integer in the range [0, vocab_size) representing a token id. 
-        If `x` has only one dimension, the batch size is assumed to be 1.
-
-        Args:
-            x: A tensor of shape (..., batch_size) containing the token ids.
-
-        Returns:
-            A tensor of shape (..., batch_size, embedding_size) containing the token embeddings.
-
-        Raises:
-            IndexError: If the token ids are outside of the range [0, vocab_size).
-        """
-        if torch.any(x < 0) or torch.any(x >= self.embedding.num_embeddings):
-            raise IndexError("Token ids are outside of the range [0, vocab_size).")
-        return self.embedding(x)
 
 class PositionalEncoding(nn.Module):
     def __init__(self, context_size: int, embedding_size: int):
